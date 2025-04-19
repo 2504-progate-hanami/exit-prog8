6f2ce83be6cabd72677234c16397de6260486999
thirdlf03 <thirdlf03@gmail.com>
2025-04-19 23:13:04 +0900

    delete: delete comment

--- app/components/SubmitButton.tsx
+++ app/components/SubmitButton.tsx
@@ -9,7 +9,7 @@ export function SubmitButton({
     <div className="relative group inline-block">
       <button
         type="button"
-        className="bg-teal-500 px-12 py-2 hover:bg-teal-600 text-white rounded" // ボタンに角丸を追加すると見栄えが良いかもしれません
+        className="bg-teal-500 px-12 py-2 hover:bg-teal-600 text-white rounded"
         onClick={onClick}
       >
         <div className="flex items-center justify-center">
@@ -18,7 +18,7 @@ export function SubmitButton({
       </button>
       <div
         className="absolute bottom-full left-1/2 transform -translate-x-1/2 mb-2 w-auto
-                   opacity-0 group-hover:opacity-100 transition-opacity duration-300 pointer-events-none" // ホバー時に表示、トランジション追加、マウスイベント無効化
+                   opacity-0 group-hover:opacity-100 transition-opacity duration-300 pointer-events-none"
       >
         <div className="bg-gray-800 text-white text-xs rounded py-1 px-3 whitespace-nowrap">
           ⌘ + Enter
