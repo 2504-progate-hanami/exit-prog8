73cb29aae06b7a221a8aa77112ddb9f8bce7430c
thirdlf03 <thirdlf03@gmail.com>
2025-04-19 23:11:36 +0900

    feat: add submit button

---
+++ app/components/SubmitButton.tsx
@@ -0,0 +1,34 @@
+import type { JSX } from "react";
+
+export function SubmitButton({
+  onClick,
+}: {
+  onClick: () => void;
+}): JSX.Element {
+  return (
+    <div className="relative group inline-block">
+      <button
+        type="button"
+        className="bg-teal-500 px-12 py-2 hover:bg-teal-600 text-white rounded" // ボタンに角丸を追加すると見栄えが良いかもしれません
+        onClick={onClick}
+      >
+        <div className="flex items-center justify-center">
+          <p>{"できた!"}</p>
+        </div>
+      </button>
+      <div
+        className="absolute bottom-full left-1/2 transform -translate-x-1/2 mb-2 w-auto
+                   opacity-0 group-hover:opacity-100 transition-opacity duration-300 pointer-events-none" // ホバー時に表示、トランジション追加、マウスイベント無効化
+      >
+        <div className="bg-gray-800 text-white text-xs rounded py-1 px-3 whitespace-nowrap">
+          ⌘ + Enter
+        </div>
+        <div
+          className="absolute left-1/2 transform -translate-x-1/2 top-full w-0 h-0
+                      border-x-4 border-x-transparent
+                      border-t-4 border-t-gray-800"
+        ></div>
+      </div>
+    </div>
+  );
+}
