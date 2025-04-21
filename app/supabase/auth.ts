4ac5358293f4770fd0ec2cd233877e0107cdf4fb
りおちん <118029.ichikama@gmail.com>
2025-04-21 21:05:06 +0900

    add: signin function

--- app/supabase/auth.ts
+++ app/supabase/auth.ts
@@ -8,9 +8,24 @@ export const signUp = async (email: string, password: string) => {
 
   if (error) {
     console.error("サインアップ失敗！:", error.message);
-    return error;
+    return { error };
   }
 
   console.log("サインアップ成功:", data);
+  return { data };
+};
+
+export const signIn = async (email: string, password: string) => {
+  const { data, error } = await supabase.auth.signInWithPassword({
+    email,
+    password,
+  });
+
+  if (error) {
+    console.error("ログイン失敗:", error.message);
+    return { error };
+  }
+
+  console.log("ログイン成功:", data);
   return { data };
 };
