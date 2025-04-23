50f8a72bfc269c9a8a26e9abe709ae0fe99f6f58
mac_kinu <techgit1024@gmail.com>
2025-04-23 21:03:37 +0900

    add: 手順コンポーネントの配置

---
+++ app/components/procedureComponent.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Card } from "~/components/ProblemCards/ProblemCardComponent";
+
+export function ProcedureComponent() {
+  return (
+    <div className="flex flex-col space-y-4 p-4 bg-gray-100 h-full">
+      <Card />
+    </div>
+  );
+}
