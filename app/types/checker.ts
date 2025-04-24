db00991a86cc2a55810df39ce368b57b8d67fe22
ulxsth <ulxsth@gmail.com>
2025-04-24 11:43:23 +0900

    chore: checker の型を types に移動

---
+++ app/types/checker.ts
@@ -0,0 +1,28 @@
+/**
+ * 静的テストは、コードを受け取ってチェックする
+ */
+export interface StaticChecker {
+  description: string;
+  check: (code: string) => boolean;
+
+  // ユーザに対して表示されるメッセージ
+  message?: string;
+}
+/**
+ * 動的テストは、出力を受け取ってチェックする
+ */
+export interface DynamicChecker {
+  description: string;
+  check: (out: string) => boolean;
+
+  // ユーザに対して表示されるメッセージ
+  message?: string;
+}
+type Checker = StaticChecker | DynamicChecker;
+/**
+ * ユーザの入力したコードに対して行われるチェックの結果を表すオブジェクト。
+ */
+export interface CheckResult {
+  status: "success" | "failed";
+  failedChecker?: Checker;
+}
