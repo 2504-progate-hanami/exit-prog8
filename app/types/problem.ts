d6a22776c4d6ab12a1a8f916fe13df08070b4942
ulxsth <ulxsth@gmail.com>
2025-04-24 11:44:57 +0900

    chore: Problem に checkers 項目を追加

--- app/types/problem.ts
+++ app/types/problem.ts
@@ -1,9 +1,15 @@
+import type { DynamicChecker, StaticChecker } from "./checker";
+
 export interface Problem {
   name: string;
   instructions: Array<ProblemInstruction>;
   procedure: HTMLElement;
   initialCode: string;
   answerCode: string;
+  checkers: {
+    static: Array<StaticChecker>;
+    dynamic: Array<DynamicChecker>;
+  };
 }
 
 export interface ProblemInstruction {
