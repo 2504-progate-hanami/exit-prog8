105fcc54cddab92e747a7095e9962e52b024cbf1
ulxsth <ulxsth@gmail.com>
2025-04-26 19:02:51 +0900

    feature: dom 操作のサンプルをかいてみた

--- app/routes/problems.tsx
+++ app/routes/problems.tsx
@@ -74,6 +74,25 @@ const Problems: React.FC = () => {
         }
       }
     })();
+
+    // DOM 更新処理
+    const elm = document.querySelector("header");
+    if (!elm) {
+      console.error("Element not found:");
+      return;
+    }
+    const styleElement = document.createElement("style");
+    styleElement.textContent = `
+      .rainbow-gradient {
+        background: linear-gradient(to right,#e60000,#f39800,#fff100,#009944,#0068b7,#1d2088,#920783,#e60000) 0 / 200%;
+        animation: 5s example4 linear infinite;
+      }
+      @keyframes example4 {
+        100% { background-position: 200%; }
+      }
+    `;
+    document.head.appendChild(styleElement);
+    elm.classList.add("rainbow-gradient");
   }, []);
 
   if (error) {
