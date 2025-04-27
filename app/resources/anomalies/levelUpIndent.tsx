bb63872321595709719fbdc7661813464016aa3d
ulxsth <ulxsth@gmail.com>
2025-04-27 12:15:30 +0900

    fix: ヘッダーが虹色にならないように修正した

--- app/resources/anomalies/levelUpIndent.tsx
+++ app/resources/anomalies/levelUpIndent.tsx
@@ -12,25 +12,7 @@ export default {
   name: "インデントが強い",
   description: "インデントが半角30になりました。",
   execute: async () => {
-    const elm = document.querySelector("header");
-    if (!elm) {
-      console.error("Element not found:");
-      return;
-    }
-    const styleElement = document.createElement("style");
-    styleElement.textContent = `
-          .rainbow-gradient {
-            background: linear-gradient(to right,#e60000,#f39800,#fff100,#009944,#0068b7,#1d2088,#920783,#e60000) 0 / 200%;
-            animation: 5s example4 linear infinite;
-          }
-          @keyframes example4 {
-            100% { background-position: 200%; }
-          }
-        `;
-    document.head.appendChild(styleElement);
-    elm.classList.add("rainbow-gradient");
-
-    // Modify Monaco Editor tab size
+    // Modify Monaco Editor tab size only
     const monacoEditor = window.monaco?.editor;
     if (monacoEditor) {
       monacoEditor.getModels().forEach((model: monaco.editor.ITextModel) => {
