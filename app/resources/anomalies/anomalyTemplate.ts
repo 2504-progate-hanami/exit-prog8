10e4451c2965752a0e0e2574b0c2b2870cd419b1
mac_kinu <techgit1024@gmail.com>
2025-04-27 12:50:38 +0900

    add: anomalyTemplate.ts

---
+++ app/resources/anomalies/anomalyTemplate.ts
@@ -0,0 +1,10 @@
+import type { Anomaly } from "~/types/anomaly";
+
+export default {
+  id: "anomalyID",
+  name: "異変の名前",
+  description: "異変の説明",
+  execute: async () => {
+    // 異変の処理
+  },
+} as Anomaly;
